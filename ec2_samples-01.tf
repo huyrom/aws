ef3c2c5652bc8d718009d547f268a64f15b0ebdb
huyrom <59599493+huyrom@users.noreply.github.com>
2020-04-07 16:14:49 +0700

    Update ec2_samples-01.tf

--- ec2_samples-01.tf
+++ ec2_samples-01.tf
@@ -2,19 +2,19 @@ provider "aws" {
   region     = "ap-southeast-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-0cbc6aae997c6538a" # Ubuntu 14.04 LTS AMD64 in eu-central-1<br>
+  ami           = "ami-0cbc6aae997c6538a" # AMI for free eligible
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.allow_8088.name}"]
 }
 # Tạo resource aws_security_group và allow_8088 và khai báo tham chiếu trên resource của aws_instance ${aws_security_group.allow_8088.name}
 
-resource "aws_security_group" "allow_8088" {<br>
-  name = "allow_ssh"<br>
-  # SSH access<br>
-  ingress {<br>
-    from_port   = 8088<br>
-    to_port     = 8088<br>
-    protocol    = "tcp"<br>
+resource "aws_security_group" "allow_8088" {
+  name = "allow_ssh"
+  # SSH access
+  ingress {
+    from_port   = 8088
+    to_port     = 8088
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
